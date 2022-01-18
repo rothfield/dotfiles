0cbb45c07e259a6bf49e54839972dfe000efa3f5
John Rothfield <johnrothfield@gmail.com>
2022-01-18 10:32:31 -0800

    x

---
+++ dot_screenlayout/executable_home_2_external_monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 1920x1200 --pos 1600x0 --rotate normal --output DP-2 --mode 1920x1200 --pos 3520x0 --rotate normal --output HDMI-2 --off
